1bc7cbf84fc2469be4ca66bd72ac1da5e143a00f
Youssef-Magdy-19 <youssefostora1155@gmail.com>
2025-08-07 15:32:37 +0300

    keys in model = keys in webhooks

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,56 +3,60 @@ import User from "../models/User.js";
 
 // API Controller Function to Manage Clerk USer with database
 
-const clerkWebhooks = async (req , res)=>{
-    try{
+const clerkWebhooks = async (req, res) => {
+    console.log('webhook receive:')
+    try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
         await whook.verify(JSON.stringify(req.body), {
-            "svix-id" : req.headers['svix-id'],
-            "svix-timestamp" : req.headers['svix-timestamp'],
-            "svix-signature" : req.headers['svix-signature'],
+            "svix-id": req.headers['svix-id'],
+            "svix-timestamp": req.headers['svix-timestamp'],
+            "svix-signature": req.headers['svix-signature'],
         })
 
-        const {data , type} = req.body
-        console.log('webhook receive:' , type)
+        const { data, type } = req.body
+        console.log('webhook receive:', type)
 
         switch (type) {
-            case 'user.created': {
-                console.log('create user:' , type)
+            case 'user.created':
+                console.log('create user:', type);
+                try {
+                    const userData = {
+                        _id: data.id,
+                        name: data.first_name,
+                        email: data.email_addresses[0].email_address,
+                        imageUrl: data.profile_image_url,
+                    };
+                    await User.create(userData);
+                    return res.status(200).json({});
+                } catch (error) {
+                    console.error('Error creating user:', error);
+                    return res.status(500).json({ error: 'Failed to create user' });
+                }
+
+            case 'user.updated': {
+                console.log('update user:', type)
                 const userData = {
-                    _id : data.id , 
-                    email : data.email_addresses[0].email_address,
-                    name : data.first_name + " " + data.last_name,
-                    imageUrl : data.imageUrl,
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.profile_image_url,
                 }
-                await User.create(userData)
+                await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
                 break;
             }
 
-            case 'user.updated' : {
-                console.log('update user:' , type)
-                const userData = { 
-                    email : data.email_addresses[0].email_address,
-                    name : data.first_name + " " + data.last_name,
-                    imageUrl : data.imageUrl,
-                }
-                await User.findByIdAndUpdate(data.id , userData)
-                res.json({})
-                break;
-            }
-
-            case 'user.deleted' : {
-                console.log('deleted user:' , type)
+            case 'user.deleted': {
+                console.log('deleted user:', type)
                 await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
             }
-        
+
             default:
                 break;
         }
     } catch (error) {
-        res.json({success : false , message : error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 export default clerkWebhooks
--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 const userShecma = new mongoose.Schema({
     _id : {type : String , require : true , trim : true} ,
-    username : {type : String , require : true , trim : true} , 
+    name : {type : String , require : true , trim : true} , 
     email : {type : String , require : true , trim : true , unique : true} ,
     imageUrl : {type : String , require : true , trim : true}, 
     enrolledCourses : [
